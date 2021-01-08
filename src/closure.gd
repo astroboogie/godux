24f10b65914d63705979c95f03275d0958764a1e
Nathaniel Adams <nathaniel.adams@berkeley.edu>
2021-01-08 11:14:20 -0800

    Added Closure class

---
+++ src/closure.gd
@@ -0,0 +1,17 @@
+# Created by xananax
+
+extends Reference
+class_name Closure 
+
+var _object
+var _method
+var _arguments
+
+func _init(object: Object, method: String, arguments:=[]) -> void:
+    assert(object.has_method(method), "Object %s doesn't have a method called %s"%[object, method])
+    _object = object
+    _method = method
+    _arguments = arguments
+
+func call_funcv(additional_arguments:=[]):
+    return _object.callv(_method, _arguments + additional_arguments)
